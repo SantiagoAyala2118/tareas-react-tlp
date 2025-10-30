9449ae965bc0a99df39383ef17b2af0fcd26cb45
Santiago Ayala <santitomayala07@gmail.com>
2025-10-29 23:51:31 -0300

    Se avanzo en gran medida con el formulario de registro

--- tlp-segunda-tarea/src/components/Register.jsx
+++ tlp-segunda-tarea/src/components/Register.jsx
@@ -14,26 +14,61 @@ import { useForm } from "../hooks/useForm";
 //   - Reseteará el formulario.
 
 export const Register = () => {
-  const { handleChange, formulario, setFormulario } = useForm();
+  const { handleChange, handleReset, formulario } = useForm({});
 
   return (
     <>
-      <form action="#">
+      <form action="#" className="register">
         <div>
           <label>Username</label>
-          <input type="text" placeholder="Ingrese su nombre de usuario" />
+          <input
+            type="text"
+            className="input"
+            name="username"
+            placeholder="Ingrese su nombre de usuario"
+          />
           <br />
           <label>Email</label>
-          <input type="text" placeholder="Ingrese su correo" />
+          <input
+            type="text"
+            className="input"
+            name="email"
+            placeholder="Ingrese su correo"
+          />
           <br />
           <label>Password</label>
-          <input type="text" placeholder="Ingrese su contraseña" />
+          <input
+            type="text"
+            className="input"
+            name="password"
+            placeholder="Ingrese su contraseña"
+          />
           <br />
           <label>Firstname</label>
-          <input type="text" placeholder="Ingrese su primer nombre" />
+          <input
+            type="text"
+            className="input"
+            name="firstname"
+            placeholder="Ingrese su primer nombre"
+          />
           <br />
           <label>Lastname</label>
-          <input type="text" placeholder="Ingrese su apellido" />
+          <input
+            type="text"
+            className="input"
+            name="lastname"
+            placeholder="Ingrese su apellido"
+          />
+        </div>
+        <div>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              handleReset();
+            }}
+          >
+            Enviar
+          </button>
         </div>
       </form>
     </>
