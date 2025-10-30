c2511601adefcec2680c764d99542b89a53cba64
Santiago Ayala <santitomayala07@gmail.com>
2025-10-29 23:57:21 -0300

    Se implentaron las funciones solicitadas en cierta medida

--- tlp-segunda-tarea/src/App.jsx
+++ tlp-segunda-tarea/src/App.jsx
@@ -1,12 +1,29 @@
-import { Login } from "./components/Login";
 import "./App.css";
+import { Login } from "./components/Login";
 import { Register } from "./components/Register";
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+
+  const handleLogin = () => {
+    setUsername(username);
+  };
+
+  const handleLogout = (user) => {
+    setUsername("");
+  };
+
   return (
     <>
+      <div>
+        <h1>Hola de nuevo {username}</h1>
+      </div>
       <Register />
-      <Login />
+      <Login onLogin={handleLogin} />
+      <div>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
     </>
   );
 }
