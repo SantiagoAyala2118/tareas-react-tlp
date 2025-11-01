2984de6d8f13c96fc62fa39e28e9eead1d80d6ee
Santiagio Ayala <santitomayala07@gmail.com>
2025-11-01 10:30:54 -0300

    Se implementó la lógica de inabilitar los botones cuando se está haciendo la petición a la API y cuando el id del personaje es 1. Así como también las clases para los estilos

--- tercera-tarea/src/pages/MultipleCustomHooks.jsx
+++ tercera-tarea/src/pages/MultipleCustomHooks.jsx
@@ -4,20 +4,35 @@ import { useFetch } from "../hooks/useFetch";
 import { Loading } from "../components/Loading";
 
 export const MultipleCustomHooks = () => {
-  const { count, handleIncrement } = useCounter(1);
+  const { count, handleIncrement, handleDecrement } = useCounter(1);
 
   const { data, isLoading } = useFetch(
     `https://thesimpsonsapi.com/api/characters/${count}`
   );
 
   return (
-    <>
+    <div className="container">
       <h1>Los Simpsons API</h1>
       <h2>Personajes</h2>
 
       <Loading onLoading={isLoading} onData={data} onCount={count} />
 
-      <button onClick={() => handleIncrement(1)}>Siguiente</button>
-    </>
+      <div className="button-container">
+        <button
+          className="character-button"
+          onClick={() => handleDecrement(1)}
+          disabled={isLoading === true || count === 1 ? true : false}
+        >
+          Anterior
+        </button>
+        <button
+          className="character-button"
+          onClick={() => handleIncrement(1)}
+          disabled={isLoading}
+        >
+          Siguiente
+        </button>
+      </div>
+    </div>
   );
 };
