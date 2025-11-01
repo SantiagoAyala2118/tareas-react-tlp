a19e402211f167805a73e30a94004eaec5952065
Santiagio Ayala <santitomayala07@gmail.com>
2025-10-31 23:00:55 -0300

    Lógica del componente Loading terminada (puesta a revisiones)

--- tercera-tarea/src/components/Loading.jsx
+++ tercera-tarea/src/components/Loading.jsx
@@ -1,3 +1,15 @@
-export const Loading = () => {
-  return <div>Loading</div>;
+import { CharacterInfo } from "./CharacterInfo";
+
+export const Loading = ({ onLoading, onData, onCount }) => {
+  return (
+    <>
+      {onLoading ? (
+        <h3>Cargando...</h3>
+      ) : (
+        <h3>
+          <CharacterInfo onData={onData} onCount={onCount} />
+        </h3>
+      )}
+    </>
+  );
 };
