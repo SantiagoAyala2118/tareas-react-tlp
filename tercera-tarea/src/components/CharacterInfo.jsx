1d55d4964e29fe2291f6d392ccc33cbec9de4bd9
Santiagio Ayala <santitomayala07@gmail.com>
2025-10-31 23:01:37 -0300

    Lógica del componente CharacterInfo terminada (puesta a revisiones)

--- tercera-tarea/src/components/CharacterInfo.jsx
+++ tercera-tarea/src/components/CharacterInfo.jsx
@@ -1,3 +1,11 @@
-export const CharacterInfo = () => {
-  return <div>CharacterInfo</div>;
+export const CharacterInfo = ({ onData, onCount }) => {
+  return (
+    <div>
+      <img
+        src={`https://cdn.thesimpsonsapi.com/200/character/${onCount}.webp`}
+        alt="Personaje de los Simpsons"
+      />
+      <h2>{onData?.name}</h2>
+    </div>
+  );
 };
