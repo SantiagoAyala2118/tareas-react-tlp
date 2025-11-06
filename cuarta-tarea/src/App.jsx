02bcdd3c5d3ce9ecf4ce6cb12ba3170c9d0e94d4
Santiagio Ayala <santitomayala07@gmail.com>
2025-11-06 09:55:07 -0300

    Ahora App solamente renderiza AppRouter

--- cuarta-tarea/src/App.jsx
+++ cuarta-tarea/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import { HomePage } from "./pages/HomePage";
+import { AppRouter } from "./routes/AppRouter";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <AppRouter />
     </>
   );
 }
